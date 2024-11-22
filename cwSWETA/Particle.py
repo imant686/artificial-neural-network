2dafa8d82d08d20751c941244785a1d4ef61d463
Iman Teh <iman.teh6868@gmail.com>
2024-11-22 12:41:54 +0000

    Added PSO and ANN into f20bc folder

--- cwSWETA/Particle.py
+++
@@ -1,8 +0,0 @@
-import numpy 
-class Particle:
-    def __init__(self,vectorSize):
-            self.particlePosition=numpy.random.rand(vectorSize)
-            self.particleVelocity=numpy.random.rand(vectorSize)
-            self.bestPosition=numpy.copy(self.particlePosition)
-
-
